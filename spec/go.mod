575cba756e90e135a09bf5374eb9695a44487042
ramonskie <ramonmakkelie@gmail.com>
2020-10-05 12:14:28 +0200

    add initial spec tests

---
+++ spec/go.mod
@@ -0,0 +1,9 @@
+module github.com/genesis-community/concourse-genesis-kit/spec
+
+go 1.14
+
+require (
+	github.com/genesis-community/testkit v0.0.0-20200910091437-6b55f6cc04b7
+	github.com/onsi/ginkgo v1.14.0
+	github.com/onsi/gomega v1.10.1
+)
---
+++ spec/spec_test.go
@@ -0,0 +1,70 @@
+package spec_test
+
+import (
+	"path/filepath"
+	"runtime"
+
+	. "github.com/genesis-community/testkit/testing"
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Jumpbox Kit", func() {
+	BeforeSuite(func() {
+		_, filename, _, _ := runtime.Caller(0)
+		KitDir, _ = filepath.Abs(filepath.Join(filepath.Dir(filename), "../"))
+	})
+
+	Describe("jumpbox", func() {
+		Test(Environment{
+			Name:        "base",
+			CloudConfig: "aws",
+			CPI:         "aws",
+			Exodus:      "cf",
+			// Focus:       true,
+		})
+		Test(Environment{
+			Name:        "base-allparams",
+			CloudConfig: "aws",
+			CPI:         "aws",
+			Exodus:      "cf",
+			// Focus:       true,
+		})
+		Test(Environment{
+			Name:        "metrics-emitter",
+			CloudConfig: "aws",
+			CPI:         "aws",
+			Exodus:      "cf",
+			// Focus:       true,
+		})
+		Test(Environment{
+			Name:        "external-rmq-lb",
+			CloudConfig: "aws",
+			CPI:         "aws",
+			Exodus:      "cf",
+			// Focus:       true,
+		})
+		Test(Environment{
+			Name:        "route-registrar",
+			CloudConfig: "aws",
+			CPI:         "aws",
+			Exodus:      "cf",
+			// Focus:       true,
+		})
+		Test(Environment{
+			Name:        "nats-tls",
+			CloudConfig: "aws",
+			CPI:         "aws",
+			Exodus:      "cf",
+			// Focus:       true,
+		})
+		// fails due to https://github.com/genesis-community/rabbitmq-genesis-kit/issues/4
+		// Test(Environment{
+		// 	Name:        "no-tls",
+		// 	CloudConfig: "aws",
+		// 	CPI:         "aws",
+		// 	Exodus:      "cf",
+		// 	Focus:       true,
+		// })
+
+	})
+})
